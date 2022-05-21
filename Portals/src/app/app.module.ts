a8c7646ab81acec6af23519f5e5ce89d96aef47e
Chamal Kaviranga <chamal@champitsolutions.com>
2022-05-21 13:18:24 +0530

    Added bootstrap

--- Portals/src/app/app.module.ts
+++ Portals/src/app/app.module.ts
@@ -2,19 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { HomeComponent } from './component/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FindComponent } from './component/find/find.component';
+import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FindComponent
+    FindComponent,
+    FullLayoutComponent
   ],
   imports: [
     BrowserModule,
-    TooltipModule.forRoot(),
+
     AppRoutingModule
   ],
   providers: [],
